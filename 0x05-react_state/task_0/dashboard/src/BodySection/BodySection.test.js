bbccbd38a07c488cb758d88f127817f609e4627b
datlearner <kundaimuchemwa456@gmail.com>
2024-08-10 11:51:40 +0200

    added a readme file

--- 0x05-react_state/task_0/dashboard/src/BodySection/BodySection.test.js
+++ 0x05-react_state/task_0/dashboard/src/BodySection/BodySection.test.js
@@ -1,27 +1,19 @@
-import PropTypes from "prop-types";
-import React, { Component } from "react";
+import React from "react";
 import BodySection from "./BodySection";
-import { StyleSheet, css } from "aphrodite";
+import { shallow } from "enzyme";
 
-class BodySectionWithMarginBottom extends Component {
-  render() {
-    return (
-      <div className={css(styles.bodySectionWithMargin)}>
-        <BodySection {...this.props} />
-      </div>
+describe("BodySection tests", () => {
+  it("should render correctly", () => {
+    const wrapper = shallow(
+      <BodySection title="test title">
+        <p>test children node</p>
+      </BodySection>
     );
-  }
-}
 
-const styles = StyleSheet.create({
-  bodySectionWithMargin: {
-    marginBottom: "40",
-  },
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.exists("h2")).toBe(true);
+    expect(wrapper.find("h2").html()).toEqual("<h2>test title</h2>");
+    expect(wrapper.exists("p")).toBe(true);
+    expect(wrapper.find("p").text()).toEqual("test children node");
+  });
 });
-
-BodySectionWithMarginBottom.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
-};
-
-export default BodySectionWithMarginBottom;
