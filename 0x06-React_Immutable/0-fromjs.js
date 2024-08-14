5eea141da0aee91b53f3ec7fff1b42190b0c43fc
datlearner <kundaimuchemwa456@gmail.com>
2024-08-14 14:21:16 +0200

    react immutable

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,15 @@
-import { fromJS } from './node_modules/immutable/dist/immutable';
+const { fromJS } = require('immutable');
 
-export default function getImmutableObject(object) {
-  return fromJS(object);
+function getImmutableObject(obj) {
+    return fromJS(obj);
 }
+
+const obj = {
+    fear: true,
+    smell: -1033575916.9145899,
+    wall: false,
+    thing: -914767132
+};
+
+const immutableMap = getImmutableObject(obj);
+console.log(immutableMap);
