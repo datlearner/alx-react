c5cf6822a8016e942b769c31c829a7c9db0eab70
datlearner <kundaimuchemwa456@gmail.com>
2024-08-16 16:01:01 +0200

    fixing lint issues

---
+++ 0x06-React_Immutable/eslint.config.mjs
@@ -0,0 +1,11 @@
+// eslint.config.js
+export default [
+  {
+    files: ["**/*.js"],
+    rules: {
+      "no-multiple-empty-lines": ["error", { max: 0 }],
+      // Add other rules as needed
+    },
+  },
+];
+
